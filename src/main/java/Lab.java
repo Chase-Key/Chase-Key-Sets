4f08e9143ad71f69ebb2d1610b120e56a5ea7b71
Chase Key <Chase.d.key@gmail.com>
2023-03-01 16:31:21 +0000

    Set methods

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -1,6 +1,8 @@
 
 import java.util.Set;
 
+import scala.collection.mutable.HashSet;
+
 /**
  * Sets are unordered collections of elements. Sets do not allow duplicates.
  *
@@ -20,7 +22,9 @@ public class Lab {
      * @return a new Set<Integer> object.
      */
     public Set<Integer> createSet(){
-        return null;
+        // HashSet<Integer> set = new HashSet<Integer>();
+        Set<Integer> set = new java.util.HashSet<>(0);
+        return set;
     }
 
     /**
@@ -29,7 +33,7 @@ public class Lab {
      * @return the size of set. (number of values)
      */
     public int getSize(Set<Integer> set){
-        return 0;
+        return set.size();
     }
 
     /**
@@ -39,7 +43,7 @@ public class Lab {
      * @return nothing, pass by reference will cause changes to the list object to be reflected across the program.
      */
     public void addUniqueItem(Set<Integer> set, int item){
-
+        set.add(item);
     }
 
     /**
@@ -49,6 +53,6 @@ public class Lab {
      * @return nothing, pass by reference will cause changes to the list object to be reflected across the program.
      */
     public void removeItem(Set<Integer> set, int item){
-
+        set.remove(item);
     }
 }
